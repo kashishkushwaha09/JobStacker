41ca719145ae4506e48eb55518904078a21be075
kashishkushwaha09 <kashishkushwaha03@gmail.com>
2025-07-29 09:39:31 +0530

    node mailer integrated

---
+++ utils/sendEmail.js
@@ -0,0 +1,29 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+const sendEmail = async (to, subject, htmlContent) => {
+  const mailOptions = {
+    from: `"JobStacker" <${process.env.EMAIL_USER}>`,
+    to,
+    subject,
+     text: "You have a new notification from JobStacker",
+    html: htmlContent,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log(`Email sent to ${to}`);
+  } catch (error) {
+    console.error("Failed to send email:", error);
+  }
+};
+
+module.exports = sendEmail;
